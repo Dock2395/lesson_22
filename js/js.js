061408121c98aef4c4597f038e2b5b5d949f9089
Dock2395 <43069756+Dock2395@users.noreply.github.com>
2022-12-10 18:50:29 +0300

    new

---
+++ js/js.js
@@ -0,0 +1,56 @@
+/* Відео 2
+  Варіант 1
+console.log('Вчимо JS')
+  Варіант 2
+console.log('JS')
+console.log('Вчимо')
+  Варіант 3
+console.log('Вчимо') console.log('JS')
+  Варіант 4
+console.log('Вчимо);
+console.log('JS');
+*/
+
+/*
+//Відео 3
+let colorEye;
+let user;
+let userName;
+
+userName = 'Вася';
+user = userName;
+
+console.log(user);
+
+__________________________________
+//Варіант 1
+let user = 'Вася' let age = 36
+//Варіант 2
+let myage = 36
+//Варіант 3
+const BLOCK_SIZE = 14 + 50;
+//Варіант 4
+const BLOCK_HEIGHN = 100;
+//Варіант 5
+if(true){
+	var size = 15;
+}
+console.log(size);
+*/
+
+//Відео 4
+//Варіант 1
+let userAge = 36;
+let userInfo = "Фрілансер $(userAge)";
+console.log(userInfo)
+// Пропущено : та не вірні лапки і дужки
+// Правильний запис let userInfo = `Фрілансер: ${userAge}`;
+//Варіант 2
+let userHight = 145 / 0;
+console.log(userHight);
+//Варіант 3
+let userName;
+console.log(typeof userName);
+//Варіант 4
+let userSize = "45" / "8";
+console.log(typeof userSize);
